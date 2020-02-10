7eec59ec15371c0b61e8e60891936bc3547c13cb
Mike Enochs <msenochs@umich.edu>
2020-02-09 22:55:34 -0500

    Installed gatsby-plugin-google-fonts
    Many of my fonts were not showing up on my prod build.  Looks like I needed a plugin.

--- portfolio-frontend/gatsby-config.js
+++ portfolio-frontend/gatsby-config.js
@@ -70,8 +70,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `Bowlby One SC`,
+          `Gudea`,
+          `Montserrat`,
+          `Quicksand`,
+          `Agency FB`,
+          `Quattrocento Sans`,
+          `Six Caps`,
+          `Fugaz One`,
+        ],
+        display: 'swap',
+      },
+    },
   ],
 };
