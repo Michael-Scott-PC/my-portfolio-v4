79a4fc042acdd8f361ab95f6bba6a45ce352d384
Mike Enochs <msenochs@umich.edu>
2020-02-12 15:12:47 -0500

    Added logo to header.js

--- portfolio-frontend/src/components/layout/header.js
+++ portfolio-frontend/src/components/layout/header.js
@@ -1,10 +1,26 @@
 import styles from '../../css/header.module.css';
 import React from 'react';
 import { Link } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
+import Img from 'gatsby-image';
 
 const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(
+    graphql`
+      {
+        file(relativePath: { eq: "portfolio-logo.png" }) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  );
+
   const toggleCollapse = () => {
     document
       .getElementById('navbarToggleExternalContent')
@@ -14,7 +30,6 @@ const Header = ({ siteTitle }) => {
   return (
     <header
       style={{
-        // marginBottom: `1.45rem`,
         position: 'sticky',
         top: '0',
         zIndex: '10000',
@@ -23,10 +38,11 @@ const Header = ({ siteTitle }) => {
     >
       <Container className={styles.headerContainer}>
         <Row className="pt-2">
-          <Col xs={3} className="d-lg-none">
+          <Col xs={2} className="d-lg-none">
             <button
               id="navbar-toggler"
               className="navbar-toggler"
+              style={{ height: '100%' }}
               type="button"
               data-target="#navbarToggleExternalContent"
               aria-controls="navbarToggleExternalContent"
@@ -40,18 +56,32 @@ const Header = ({ siteTitle }) => {
             </button>
           </Col>
 
-          <Col xs={9} lg={3} className="m-auto">
+          <Col xs={8} lg={3} className="m-auto text-center">
             <h1 style={{ margin: 0, fontSize: '1.5em' }}>
               <Link to="/" className={`${styles.siteTitle}`}>
                 {siteTitle}
               </Link>
             </h1>
           </Col>
+          <Col xs={2} className="d-lg-none">
+            <Img
+              fluid={data.file.childImageSharp.fluid}
+              style={{
+                objectFit: 'contain !important',
+              }}
+              className=" mt-2"
+            />
+          </Col>
           <Col
             lg={9}
             className={`${styles.linkContainer} text-center collapse`}
             id="navbarToggleExternalContent"
           >
+            <Img
+              fluid={data.file.childImageSharp.fluid}
+              style={{ width: '7%' }}
+              className="d-none d-lg-inline mr-5"
+            />
             <Link to="/" className={`${styles.dropDownItem}`}>
               Home
             </Link>
